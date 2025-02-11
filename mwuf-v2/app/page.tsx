e3884b781a8f744eb059b258812d0c2873fbb928
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-02-11 22:01:29 +0800

    Implemented browser icon + logo

--- mwuf-v2/app/page.tsx
+++ mwuf-v2/app/page.tsx
@@ -4,11 +4,12 @@ export default function Home() {
   return (
     <div>
       <Image 
-       src = "/images/mwuf_logo.png"
-       width = {500}
-       height = {500}
-       alt = "logo"
-       />
+        className = ""
+        src = "/images/mwuf_logo.png"
+        width = {500}
+        height = {500}
+        alt = "logo"
+        />
     </div>
   );
 }
