c84ca1e83fc0326379e8a0cdb406449439440487
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-02-13 07:43:18 +0800

    Installed shadcn ui.

--- mwuf-v2/app/page.tsx
+++ mwuf-v2/app/page.tsx
@@ -1,15 +1,22 @@
+'use client';
 import Image from "next/image"
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div>
-      <Image 
-        className = ""
-        src = "/images/mwuf_logo.png"
-        width = {500}
-        height = {500}
-        alt = "logo"
-        />
+      <div className="h-screen flex flex-col items-center pt-10">
+        <Image 
+          className = ""
+          src = "/images/mwuf_logo.png"
+          width = {400}
+          height = {400}
+          alt = "logo"
+          />
+        <Button>Hello</Button>
+      </div>
+      
     </div>
   );
 }
