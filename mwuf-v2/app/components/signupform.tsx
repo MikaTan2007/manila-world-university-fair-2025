34dee301b1404635198c5d5e98bfc3b41ceee16d
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-02-13 17:24:55 +0800

    Created password verification

--- mwuf-v2/app/components/signupform.tsx
+++ mwuf-v2/app/components/signupform.tsx
@@ -1,9 +1,49 @@
+//React
+import { SetStateAction, useState, useEffect} from "react";
+
+//Lucide
+import { EyeClosed, Eye, CircleCheck, CircleX } from "lucide-react";
+
+//Shadcn
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const SignUpForm: React.FC = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setConfirmPassword] = useState(false);
+
+    const [firstPassword, setFirstPassword] = useState("");
+    const [secondPassword, setSecondPassword] = useState("");
+
+    const [samePassword, setSamePassword] = useState(true);
+
+    const handleFirstPasswordChange = (e:  React.ChangeEvent<HTMLInputElement>) => {
+        setFirstPassword(e.target.value);
+    }
+
+    const handleSecondPasswordChange = (e:  React.ChangeEvent<HTMLInputElement>) => {
+        setSecondPassword(e.target.value);
+    }
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+        
+    }
+
+    const toggleConfirmPasswordVisibility = () => {
+        setConfirmPassword(!showConfirmPassword);
+    }
+
+    useEffect(() => {
+        if (firstPassword == secondPassword) {
+            setSamePassword(true)
+        } else {
+            setSamePassword(false);
+        }
+    }, [firstPassword, secondPassword]); 
+
     return(
         <Card className = "mx-auto max-w-sm">
             <CardHeader className = "space-y-1">
@@ -25,8 +65,24 @@ const SignUpForm: React.FC = () => {
                         <Label htmlFor="password">
                             Password
                         </Label>
-                        <Input id = "password" type = "password" placeholder = "Input Password" required></Input>
-                        <Input id = "password" type = "password" placeholder="Confirm Password" required></Input>
+                        <div className = "flex">
+                            <Input value = {firstPassword} onChange = {handleFirstPasswordChange} className="border-0" id = "password" type = {showPassword ? "text" : "password"} placeholder = "Input Password" required></Input>
+                            <Button onClick = {togglePasswordVisibility} variant = "ghost" size = "icon">
+                                {showPassword ? <Eye></Eye> : <EyeClosed></EyeClosed>}
+                            </Button>
+                        </div>
+                        
+                        <div className = "flex">
+                            <Input value = {secondPassword} onChange = {handleSecondPasswordChange} className = "border-0" id = "password" type = {showConfirmPassword ? "text" : "password"} placeholder = "Confirm Password" required></Input>
+                            <Button onClick = {toggleConfirmPasswordVisibility} variant = "ghost" size = "icon">
+                                {showConfirmPassword ? <Eye></Eye> : <EyeClosed></EyeClosed>}
+                            </Button>
+                        </div>
+
+                        <div className = "text-sm flex">
+                            {samePassword ? <CircleCheck color = "green" className = "size-5 animate-pulse"></CircleCheck> : <CircleX color = "red" className = "size-5 animate-pulse"></CircleX>}
+                            {samePassword ? <p className = "text-green-700 animate-pulse">Passwords match</p> : <p className = "text-red-500 animate-pulse">Passwords don't match</p>}
+                        </div>
                     </div>
 
                     <Button type = "submit" variant = "ghost" className = "w-full text-white bg-blue-500">
--- mwuf-v2/app/page.tsx
+++ mwuf-v2/app/page.tsx
@@ -8,11 +8,12 @@ export default function Home() {
     <div>
       <div className="flex flex-col items-center pt-10">
         <Image 
-          className = ""
+          className = "w-auto h-auto"
           src = "/images/mwuf_logo.png"
           width = {400}
           height = {400}
           alt = "logo"
+          priority={true}
           />
       </div>
       <div className = "pt-10">
