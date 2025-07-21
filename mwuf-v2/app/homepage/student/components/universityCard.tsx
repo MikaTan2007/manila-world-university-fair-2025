9fb120ec57c8ba7aff2f782e37de928ecfc6d783
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-07-21 15:53:00 +0800

    Reapply "Added grid view skeleton load student homepge"
    This reverts commit d94217e830daa1fd21fba79061ddb2bf101dd38d.

--- mwuf-v2/app/homepage/student/components/universityCard.tsx
+++ mwuf-v2/app/homepage/student/components/universityCard.tsx
@@ -1,6 +1,5 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label";
 
 interface UniversityProps {
     university: {
@@ -18,7 +17,7 @@ interface UniversityProps {
 
 export function UniversityCard({ university }: UniversityProps) {
     return (
-        <Card className="w-full hover:shadow-lg transition-shadow font-serif">
+        <Card className="w-full hover:shadow-lg transition-shadow font-mons">
             <CardHeader>
                 <CardTitle className="text-2xl font-bold flex justify-start text-green-800">{university.uni_name}</CardTitle>
                 <CardDescription className="font-bold">
