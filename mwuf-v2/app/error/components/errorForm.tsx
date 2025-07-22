24eb34dfc7413416ecacb80507f904af0d291303
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-07-22 22:38:43 +0800

    Created error form

---
+++ mwuf-v2/app/error/components/errorForm.tsx
@@ -0,0 +1,53 @@
+import {useRouter} from "next/navigation";
+
+//Shadcn
+import { Card, 
+        CardHeader, 
+        CardTitle, 
+        CardDescription, 
+        CardContent, 
+        CardFooter} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const ErrorForm: React.FC = () => {
+    //Initialization of router
+    const router = useRouter();
+
+    const goBack = async (e: React.FormEvent) => {
+        e.preventDefault();
+        router.back()
+    }
+
+    const goToHome = async (e: React.FormEvent) => {
+        e.preventDefault();
+        router.push("/")
+    }
+
+    return(
+        <Card className = "mx-auto max-w-sm">
+            <CardHeader className = "space-y-1">
+                <CardTitle className = "text-2xl font-bold flex justify-center">
+                    Error 404
+                </CardTitle>
+                <CardDescription className = "flex justify-center text-center">We apologize for the inconvenience</CardDescription>
+            </CardHeader>
+            
+            <CardContent>
+                <div className = "space-y-4">
+
+                    <Button type = "submit" onClick = {goToHome} variant = "ghost" className = "w-full text-white bg-red-400">
+                        Go to Home
+                    </Button>
+                    <Button type = "submit" onClick = {goBack} variant = "ghost" className = "w-full text-white bg-red-400">
+                        Back
+                    </Button>
+                    
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default ErrorForm
---
+++ mwuf-v2/app/error/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import Image from "next/image"
+import ErrorForm from "./components/errorForm";
+
+
+export default function StudentSignUp() {
+  return (
+    <div>
+      <div className="flex flex-col items-center pt-10">
+        <Image 
+          className = "w-auto h-auto"
+          src = "/images/mwuf_logo.png"
+          width = {400}
+          height = {400}
+          alt = "logo"
+          priority={true}
+          />
+      </div>
+      <div className = "pt-10">
+        <ErrorForm></ErrorForm>
+      </div>
+      
+    </div>
+  );
+}
