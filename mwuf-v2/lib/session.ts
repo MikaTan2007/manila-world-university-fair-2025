aab5e6878f6925f5f7e8b6f6d71b51d44187422b
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-08-07 16:29:42 +0800

    Updated admin session option

--- mwuf-v2/lib/session.ts
+++ mwuf-v2/lib/session.ts
@@ -1,6 +1,6 @@
-const sessions = new Map<string, {email: string; userType: 'student' | 'university'}>();
+const sessions = new Map<string, {email: string; userType: 'student' | 'university' | 'admin'}>();
 
-export function createSession(email: string, userType: 'student' | 'university') {
+export function createSession(email: string, userType: 'student' | 'university' | 'admin') {
     const sessionId = Math.random().toString(36).substring(2,15);
     sessions.set(sessionId, {email, userType});
     return sessionId;
