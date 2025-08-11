dd277343b8e947949bbafee814bb9fea3a024088
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-08-11 12:15:01 +0800

    Added admin student page.tsx

---
+++ mwuf-v2/app/homepage/admin/students/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+import Image from "next/image"
+import { AdminSidebar } from "../components/adminsidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function AdminStudentPage() {
+
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full">
+        <AdminSidebar/>
+
+        <main className = "flex-1 flex flex-col">
+          <div className = "sticky top-0 z-50 flex items-center p-4 bg-forest-green shadow-lg">
+              <SidebarTrigger/>
+              <h1 className="ml-4 text-xl font-semibold font-sans">Admin Student Page</h1>
+          </div>
+
+          {/* Page Content*/ }
+
+          <div>
+            <div className="flex flex-col items-center pt-10">
+              <Image 
+                className = "w-auto h-auto"
+                src = "/images/mwuf_logo.png"
+                width = {400}
+                height = {400}
+                alt = "logo"
+                priority={true}
+                />
+            </div>
+            <div className = "pt-10">
+
+            </div>
+          </div>
+        </main>
+      </div>
+    </SidebarProvider>
+  );
+}
